9e2dd8e6a03902f40856d8d263f25c15fb36b072
Khaled Adad <khaledadad@lewisu.edu>
2022-01-02 04:52:04 -0800

    CDN works fine I needed to add background method

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup() {
     // Setup code goes here
     // TODO: Create a canvas that is 400px wide and 400px tall
     createCanvas(400, 400)
+    background(Math.floor(Math.floor(Math.random() * 256)))
 }
 
 function draw() {
